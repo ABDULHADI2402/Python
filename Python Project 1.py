0c62dd6546f27271e829060068c6da6f6088e58a
ABDULHADI2402 <85433925+ABDULHADI2402@users.noreply.github.com>
2021-06-06 14:56:04 +0530

    Program-0

---
+++ Python Project 1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  6 14:03:56 2021
+
+@author: Hadi
+"""
+
+X=[1,2,3,4,5]
+Y=[2,4,6,8,10]
+print(X)
+print(Y)
+import matplotlib.pyplot as pit
+pit.plot(X,Y)
